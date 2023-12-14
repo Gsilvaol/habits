9abba7e8300e64617b1d595074686ad40b2b780d
Gustavo Silva de Oliveira <gustavosilva1994_@hotmail.com>
2023-12-14 14:10:36 -0300

    criação novo hábito

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,9 @@
 import dayjs from 'dayjs'
 import { FastifyInstance } from 'fastify'
-import { z } from 'zod'
+import { date, z } from 'zod'
 import { prisma } from "./lib/prisma"
 export async function appRoutes(app: FastifyInstance) {
-    app.get('/habits', async (request) => {
+    app.post('/habits', async (request) => {
         const createHabitBody = z.object({
             title: z.string(),
             weekDays: z.array(
@@ -13,7 +13,7 @@ export async function appRoutes(app: FastifyInstance) {
 
         const { title, weekDays } = createHabitBody.parse(request.body)
 
-const today = dayjs().startOf('day').toDate()
+        const today = dayjs().startOf('day').toDate()
 
         await prisma.habit.create({
             data: {
@@ -28,5 +28,47 @@ const today = dayjs().startOf('day').toDate()
                 }
             }
         })
+    })
+
+    app.get('/day', async (Request) => {
+        const getDayParams = z.object({
+            date: z.coerce.date()
+        })
+
+        const { date } = getDayParams.parse(Request.query)
+
+        const parsedDate = dayjs(date).startOf('day')
+        const weekDay = parsedDate.get('day')
+
+        const possibleHabits = await prisma.habit.findMany({
+            where: {
+                created_at: {
+                    lte: date,
+                },
+                weekDays: {
+                    some: {
+                        week_day: weekDay,
+                    }
+                }
+            }
+        })
+
+        const day = await prisma.day.findUnique({
+            where: {
+                date: parsedDate.toDate(),
+            },
+            include: {
+                dayHabits: true,
+            }
+        })
+
+        const completedHabits = day?.dayHabits.map(dayHabit => {
+            return dayHabit.habit_id
+        })
+
+        return {
+            possibleHabits,
+            completedHabits,
+        }
     })
 }
