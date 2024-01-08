d64637bcf4a04f67d9737e01be9736aca56a344a
Gustavo Silva de Oliveira <gustavosilva1994_@hotmail.com>
2024-01-08 17:14:26 -0300

    componente de checkbox

--- mobile/src/screens/New.tsx
+++ mobile/src/screens/New.tsx
@@ -1,7 +1,20 @@
 import { ScrollView, Text, TextInput, View } from "react-native";
 import { BackButton } from "../components/BackButton";
+import { Checkbox } from "../components/Checkbox";
+import { useState } from "react";
 
+const availableWeekDays = ['Domingo', 'Segunda-Feira', 'Terça-Feira', 'Quarta-Feira', 'Quinta-Feira', 'Sexta-Feira', 'Sábado'];
 export function New() {
+    const [weekDays, setWeekDays] = useState<number[]>([]);
+
+    function handleToggleWeekDay(weekDayIndex: number) {
+        if (weekDays.includes(weekDayIndex)) {
+            setWeekDays(prevState => prevState.filter(weekDay => weekDay !== weekDayIndex));
+        } else {
+            setWeekDays(prevState => [...prevState, weekDayIndex]);
+        }
+    }
+
     return (
         <View className="flex-1 bg-background px-8 pt-16">
             <ScrollView showsVerticalScrollIndicator={false}>
@@ -16,9 +29,23 @@ export function New() {
                 </Text>
 
                 <TextInput
-                className=" h-12 pl-4 rounded-lg mt-3 bg-zinc-800 text-white focus:border-2 focus:border-green-600"
+                    className=" h-12 pl-4 rounded-lg mt-3 bg-zinc-800 text-white focus:border-2 focus:border-green-600"
                 />
 
+                <Text className="font-semibold mt-4 mb-3 text-white text-base">
+                    Qual a recorrência?
+                </Text>
+
+                {
+                    availableWeekDays.map((weekDay, index) => (
+                        <Checkbox
+                            key={weekDay}
+                            title={weekDay}
+                            checked={weekDays.includes(index)}
+                            onPress={() => handleToggleWeekDay(index)}
+                        />
+                    ))
+                }
             </ScrollView>
         </View>
     );
