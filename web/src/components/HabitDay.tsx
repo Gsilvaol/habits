795ee944304ea45f32ac823931517d6d7d1cb0ad
Gustavo Silva de Oliveira <gustavosilva1994_@hotmail.com>
2024-01-08 13:55:41 -0300

    add readme

--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -1,25 +1,32 @@
 import * as Popover from '@radix-ui/react-popover';
+import clsx from 'clsx';
 import { ProgressBar } from './ProgressBar';
 
-//interface HabitDayProps {
-//completed: number
-//}
+interface HabitDayProps {
+  completed: number
+  amount: number
+}
 
-export function HabitDay() {
+export function HabitDay({ completed, amount }: HabitDayProps) {
+  const completedPercentage = Math.round((completed / amount) * 100)
   return (
     <Popover.Root>
-      <Popover.Trigger className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg" />
+      <Popover.Trigger
+        className="{clsx('w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg' {
+          'bg-violet-500': completedPercentage >= 80,
+        })}
+      />
 
       <Popover.Portal>
         <Popover.Content className="min-w-[320px] p-6 rounded-2xl bg-zinc-900 flex flex-col">
-          <span className=" font-semibold text-zinc-400">terça-feira</span>
-          <span className="mt-1 font-extrabold leading-tight text-3x1">05/01</span>
+      <span className="font-semibold text-zinc-400">terça-feira</span>
+      <span className="mt-1 font-extrabold leading-tight text-3x1">05/01</span>
 
-          <ProgressBar progress={40} />
+      <ProgressBar progress={completedPercentage} />
 
-          <Popover.Arrow height={8} width={16} className="fill-zinc-900" />
-        </Popover.Content>
-      </Popover.Portal>
-    </Popover.Root>
+      <Popover.Arrow height={8} width={16} className="fill-zinc-900" />
+      </Popover.Content>
+      </Popover.Portal >
+    </Popover.Root >
   )
 }
--- web/src/components/SumaryTable.tsx
+++ web/src/components/SumaryTable.tsx
@@ -28,7 +28,7 @@ export function SumaryTable() {
 
             <div className="grid grid-rows-7 grid-flow-col gap-3">
                 {sumaryDates.map(date => {
-                    return <HabitDay key={date.toString()} />
+                    return <HabitDay amount={5} completed={4} key={date.toString()} />
                 })}
 
                 {amountOfDaysToFill > 0 && Array.from({ length: amountOfDaysToFill }).map((_, i) => {
