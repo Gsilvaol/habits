9d8d786588fa008ce8ba3424592aaf440b601c64
Gustavo Silva de Oliveira <gustavosilva1994_@hotmail.com>
2024-01-11 16:02:03 -0300

    criação de um novo hábito

--- web/src/components/NewHabitForm.tsx
+++ web/src/components/NewHabitForm.tsx
@@ -1,6 +1,7 @@
 import { Check } from "phosphor-react";
 import * as Checkbox from '@radix-ui/react-checkbox';
 import { FormEvent, useState } from "react";
+import { api } from "../lib/axios";
 
 const availableWeekDays = [
     'Domingo',
@@ -15,23 +16,34 @@ export function NewHabitForm() {
     const [title, setTitle] = useState('')
     const [weekDays, setWeekDays] = useState<number[]>([])
 
-    function createNewHabit(event: FormEvent) {
+    async function createNewHabit(event: FormEvent) {
         event.preventDefault()
 
-        console.log(title, weekDays)
+        if (!title || weekDays.length === 0) {
+            return
+        }
 
+        await api.post('habits', {
+            title,
+            weekDays,
+        })
+
+        setTitle('')
+        setWeekDays([])
+
+        alert('Hábito criado com sucesso!')
     }
 
     function handleToggleWeekDay(weekDay: number) {
         if (weekDays.includes(weekDay)) {
             const weekDaysWithRemovedOne = weekDays.filter(day => day != weekDay)
-            
+
             setWeekDays(weekDaysWithRemovedOne)
-        }else {
+        } else {
             const weekDaysWithAddedOne = [...weekDays, weekDay]
-            
+
             setWeekDays(weekDaysWithAddedOne)
-            
+
         }
     }
 
@@ -47,6 +59,7 @@ export function NewHabitForm() {
                 placeholder="ex: Exercicios, dormir bem, etc..."
                 className="p-4 rounded-lg mt-3 bg-zinc-800 text-white placeholder:text-zinc-400"
                 autoFocus
+                value={title}
                 onChange={event => setTitle(event.target.value)}
             />
 
@@ -60,9 +73,8 @@ export function NewHabitForm() {
                         <Checkbox.Root
                             key={weekDay}
                             className="flex items-center gap-3 group"
-                            onCheckedChange={() => {
-                                handleToggleWeekDay(index)
-                            }}
+                            checked={weekDays.includes(index)}
+                            onCheckedChange={() => handleToggleWeekDay(index)}
                         >
 
                             <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500">
